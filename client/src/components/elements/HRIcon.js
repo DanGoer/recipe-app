17c137e94bb227ac07854e0b2321a93c90958998
David G <danielgoe1983@gmail.com>
2022-06-03 15:45:37 +0200

    Added reusable hr with icons

---
+++ client/src/components/elements/HRIcon.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+function HRIcon({ view, path }) {
+  return (
+    <div className="w-4/5  ">
+      <hr className="hr-setup mb-[-26px] border-backgroundLightOn dark:border-backgroundDarkOn" />
+      <div className="flex flex-row justify-center items-center">
+        <AnimatePresence>
+          <motion.div
+            initial={{ x: -150 }}
+            animate={{ x: 0 }}
+            transition={{ ease: "easeIn", duration: 0.5 }}
+          >
+            <svg
+              className="w-10 fill-backgroundLightOn dark:fill-backgroundDarkOn"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox={view}
+            >
+              {path}
+            </svg>
+          </motion.div>
+          <motion.div
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{ ease: "easeIn", duration: 0.5 }}
+          >
+            <svg
+              className="w-14 mx-4 mb-1 fill-backgroundLightOn dark:fill-backgroundDarkOn"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox={view}
+            >
+              {path}
+            </svg>
+          </motion.div>
+          <motion.div
+            initial={{ x: 150 }}
+            animate={{ x: 0 }}
+            transition={{ ease: "easeIn", duration: 0.5 }}
+          >
+            <svg
+              className="w-10 fill-backgroundLightOn dark:fill-backgroundDarkOn"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox={view}
+            >
+              {path}
+            </svg>
+          </motion.div>
+        </AnimatePresence>
+      </div>
+      <hr className="hr-setup border-backgroundLightOn dark:border-backgroundDarkOn" />
+    </div>
+  );
+}
+
+export default HRIcon;
