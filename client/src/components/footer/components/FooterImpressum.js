e1c14d7f85df7522d842dcdb8114e5b995fdc30f
David G <danielgoe1983@gmail.com>
2022-06-16 14:48:43 +0200

    Changed HTML structure and minor bug fix

--- client/src/components/footer/components/FooterImpressum.js
+++ client/src/components/footer/components/FooterImpressum.js
@@ -6,18 +6,18 @@ function FooterImpressum() {
       <span className="flex flex-col gap-4 w-full">
         <h3>According to § 5 TMG:</h3>
         <hr className="hr-setup" />
-        <p>
+        <address>
           D. Goergens
           <br />
           Dorstener Strasse 534
           <br />
           46119 Oberhausen
-        </p>
+        </address>
       </span>
       <span className="flex flex-col gap-4 w-full">
         <h3>Contact:</h3>
         <hr className="hr-setup" />
-        <p>E-Mail: d.goergens@gmail.com</p>
+        <address>E-Mail: d.goergens@gmail.com</address>
         <FooterSocialIcons />
       </span>
     </section>
