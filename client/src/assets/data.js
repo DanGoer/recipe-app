93811f1315b8701c8abd4f0d016838d3205c407a
DanGoer <87978392+DanGoer@users.noreply.github.com>
2023-01-09 15:37:15 +0100

    changed data

--- client/src/assets/data.js
+++ client/src/assets/data.js
@@ -22,7 +22,7 @@ export const socialIconsList = [
     name: "Email",
     viewBox: "0 0 512 512",
     svg: "M464 64C490.5 64 512 85.49 512 112C512 127.1 504.9 141.3 492.8 150.4L275.2 313.6C263.8 322.1 248.2 322.1 236.8 313.6L19.2 150.4C7.113 141.3 0 127.1 0 112C0 85.49 21.49 64 48 64H464zM217.6 339.2C240.4 356.3 271.6 356.3 294.4 339.2L512 176V384C512 419.3 483.3 448 448 448H64C28.65 448 0 419.3 0 384V176L217.6 339.2z",
-    link: "mailto:dgoergens@gmail.com",
+    link: "mailto:danielgoe1983@gmail.com",
     alt: "Send Email button",
   },
   {
@@ -65,4 +65,13 @@ export const statisticsSvg = [
 // Firebase base Url for storage
 
 export const firebaseBaseUrl =
-  "https://firebasestorage.googleapis.com/v0/b/recipe-app-auth-ba48b.appspot.com/o/";
+  "https://firebasestorage.googleapis.com/v0/b/foody-reciper.appspot.com/o/";
+
+export const impressumData = {
+  address: {
+    name: "Daniel Goergens",
+    street: "Dorstener Strasse 534",
+    city: "46119 Oberhausen",
+  },
+  contact: { email: "danielgoe1983@gmail.com" },
+};
